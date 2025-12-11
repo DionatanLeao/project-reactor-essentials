8b779c7bf8eab9d98c683680f679876853f82d60
DionatanLeao <dionatan.ti@outlook.com>
2025-12-11 13:41:35 -0300

    feat: Subscribing to Mono

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -2,6 +2,8 @@ package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 /**
@@ -22,7 +24,14 @@ import org.junit.jupiter.api.Test;
 public class MonoTest {
 
     @Test
-    public void test(){
-        log.info("Everything working as intended");
+    public void MonoSubscriber() {
+        String name = "William Suane";
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe();
+        log.info("-----------------------------");
+        StepVerifier.create(mono)
+            .expectNext(name)
+                .verifyComplete();
     }
 }
