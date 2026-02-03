4db70f21aad333c622dc65124b89b6b9f747d0e1
DionatanLeao <dionatan.ti@outlook.com>
2026-02-03 20:36:00 -0300

    feat: Avoiding blocking calls with

--- src/test/java/academy/devdojo/reactive/test/FluxTest.java
+++ src/test/java/academy/devdojo/reactive/test/FluxTest.java
@@ -1,20 +1,48 @@
 package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxProcessor;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 public class FluxTest {
+    @BeforeAll
+    public static void setUp() {
+        BlockHound.install();
+    }
+
+    @Test
+    public void blockHoundWorks() throws TimeoutException, InterruptedException {
+        try {
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0);
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
     @Test
     public void fluxSubscriber() {
         Flux<String> fluxString = Flux.just("William", "Suane", "DevDojo", "Academy")
@@ -205,7 +233,7 @@ public class FluxTest {
                 .create(connectableFlux)
                 .then(connectableFlux::connect)
                 .thenConsumeWhile(i -> i <= 5)
-                .expectNext(6,7,8,9,10)
+                .expectNext(6, 7, 8, 9, 10)
                 .expectComplete()
                 .verify();
     }
@@ -221,7 +249,7 @@ public class FluxTest {
         StepVerifier
                 .create(autoConnect)
                 .then(autoConnect::subscribe)
-                .expectNext(1,2,3,4,5)
+                .expectNext(1, 2, 3, 4, 5)
                 .expectComplete()
                 .verify();
     }
